6e27317c2ad6fe115a0666e417e8a58f34f7e161
jennyliang220 <jiaojiaomao220@163.com>
2018-03-14 18:33:53 +0800

    feat: 丰富调起逻辑

--- src/mip-app-banner/README.md
+++ src/mip-app-banner/README.md
@@ -1,6 +1,6 @@
 # mip-app-banner App 调起组件
 
-用于调起 App。
+弹出【在 App 中打开】提示通栏，用户点击时调起原生 App，调起失败后跳转到下载页。
 
 标题|内容
 ----|----
@@ -11,15 +11,57 @@
 ## 示例
 
 ### 基本用法
+
+`android-scheme` 填写安卓调起App链接，`android-download`填写安卓调起失败后下载链接。
+
 ```html
 <head>
-    <meta name="apple-itunes-app" content="app-id=xxxx, app-argument=medium://xxxx">
-    <link rel="manifest" href="xxxx/manifest">
+    <!--iOS App Store 下载配置-->
+    <meta name="apple-itunes-app" content="app-id=0000000, app-argument=mipapp://open">
 </head>
-...
-<mip-app-banner id="my-app-banner" layout="nodisplay">
-    <button open-button>打开app</button>
-</mip-app-banner>
+<body>
+    <mip-app-banner id="my-app-banner" layout="nodisplay">
+        <script type="application/json">
+            {
+                "android-scheme": "mipapp://open",
+                "android-download": "https://m.mipapp.com/download"
+            }
+        </script>
+        <span open-button>打开app</span>
+    </mip-app-banner>
+</body>
+```
+
+### 使用更多配置项
+
+目前业界 App 调起的方法很多，示例中的配置对应不同的调起方式，选择合适的即可。如果不填写，将自动回退为 scheme 调起。
+
+```html
+<head>
+    <!--iOS App Store 下载配置-->
+    <meta name="apple-itunes-app" content="app-id=0000000, app-argument=mipapp://open">
+    <!--Andriod 系统通栏配置，要求支持 HTTPs 访问-->
+    <link rel="manifest" href="https://xxxx/manifest">
+</head>
+<body>
+    <mip-app-banner id="my-app-banner" layout="nodisplay">
+        <script type="application/json">
+            {
+                "ios-universal-link": "https://m.mipapp.com/open?xxx",
+
+                "android-scheme": "mipapp://open",
+                "android-download": "https://m.mipapp.com/download",
+                "android-app-link" : "https://m.mipapp.com/open?xxx",
+                "android-chrome-intent" : "Intent://mipapp/xxxx",
+
+                "wechat-open-safari": "https://oia.zhihu.com/answers/319470613",
+                "wechat-ios-yyb": "http://a.app.qq.com/o/simple.jsp?pkgname=com.baidu.searchbox",
+                "wechat-android-yyb": "",
+            }
+        </script>
+        <span open-button>打开app</span>
+    </mip-app-banner>
+</body>
 ```
 
 ## 属性
@@ -46,12 +88,75 @@
 说明：调起 app 的按钮，必须带有 open-button 属性  
 必选项：是  
 类型：html 节点  
-取值范围：无  
-单位：无  
 默认值：`<button open-button>`
+
+
+## 配置说明
+
+### iOS 系统配置
+
+#### iOS URL Scheme 调起
+字段名：`<meta name="apple-itunes-app">`
+说明：[iOS URL Scheme](https://developer.apple.com/library/content/featuredarticles/iPhoneURLScheme_Reference/Introduction/Introduction.html) 普遍用于在 iOS 系统中应用之间的调起。需要在页面`head`标签中添加 `<meta name="apple-itunes-app">` 配置，safari 中会自动弹出系统横幅。
+取值：参考上方示例
+是否必填：是
+
+#### iOS Universal Link 调起
+字段名：ios-universal-link
+说明：[Universal Link](https://developer.apple.com/library/content/documentation/General/Conceptual/AppSearch/UniversalLinks.html) 是苹果针对 iOS9+ 系统推出的应用调起方法。
+
+### Android 系统配置
+
+#### Manifest 调起（Android）
+说明：[manifest.json](https://developer.mozilla.org/en-US/docs/Web/Manifest) 主要用于 Web App 配置，`related_applications` 字段用于配置对应的原生 App，文件储存在开发者服务器上。Andriod Chrome 等浏览器会读取配置，在浏览器中显示原生的应用调起横幅。
+字段名：`<link rel="manifest">`
+取值：参考上方示例
+链接内容：参考文末 `manifest.json 示例`
+是否必填：否，但与 `android-scheme` 必须二选一。
+
+#### Android Scheme 调起（Deep Link）
+字段名：android-scheme
+说明：[Deep Link](https://developer.android.com/training/app-links/deep-linking.html) 是安卓通用的调起方法，可直接定位到应用的特定位置。  
+取值：链接以 `http://`、`https://` 或自定义字段如 `mipapp://` 开头。
+是否必填：否，但与 Manifest 必须二选一。
+
+#### App 下载链接（Android）
+字段名：android-download
+说明：当用户未安装 App 时调起失败，跳转到 `android-download` 指向的 App 下载页面。当`Manifest 调起`中`install`参数无效时，自动使用 `android-download` 作为后备跳转链接。
+取值：链接以 `http://`、`https://` 开头。
+是否必填：否，建议填写。
+
+#### App Link（Android）
+字段名：android-app-link
+说明：[App Link](https://developer.android.com/training/app-links/index.html) 是安卓针对系统版本 6.0 及以上的机型推出的调起方法，对标 iOS 的 Universal Link。链接以 http:// 或 https:// 开头。  
+取值：合法URL
+是否必填：否
+
+#### Chrome Intent（Android）
+字段名：android-chrome-intent
+说明：[Chrome Intent](https://developer.chrome.com/multidevice/android/intents) 是通过 Android Intent 技术，在 Android Chrome 25+ 中实现应用调起的方法。在页面中填写一个[链接](https://developer.chrome.com/multidevice/android/intents)，就能够完成调起 App，调起失败跳转 App 下载页等功能。
+取值：参考 [intent 文档](https://developer.chrome.com/multidevice/android/intents)
+
+### 针对腾讯部分浏览器的特殊配置
+
+腾讯 QQ 聊天 App，微信 App 屏蔽了应用调起方式。目前在 iOS 中只有两种办法解决：提示用户在safari中打开页面，或跳转到[腾讯应用宝](http://wiki.open.qq.com/wiki/%E7%A7%BB%E5%8A%A8%E5%BA%94%E7%94%A8%E6%8E%A5%E5%85%A5%E6%96%B0%E6%89%8B%E6%8C%87%E5%BC%95)链接。而在安卓系统中只能配置腾讯应用宝链接。
+
+#### 提醒用户在safari中打开
+字段名：wechat-open-safari
+取值：合法URL。落地页需要判断是否在微信中，在微信中显示【点击右上角，选择在safari中打开】。
+默认值： 无
+
+#### iOS 应用宝链接
+字段名：wechat-ios-yyb
+是否必填：否
+
+#### 安卓应用宝链接
+字段名：wechat-android-yyb
+是否必填：否
 
 ## 浏览器兼容性
 
+<!-- TODO 需要修改，这写的啥 -->
 浏览器|Android+chrome|Android+baidu|IOS+safari |其他情况
 ---|---|---|---|---
 结果页打开|支持|不支持屏蔽|不支持，暂时屏蔽|支持
@@ -60,20 +165,21 @@
 
 ## 注意事项
 
-- 在引用组件的页面头部head标签中需要加两个标签
+### 校验规则
 
-	- IOS 使用：`<meta name="apple-itunes-app" content="app-id=app的id, app-argument=medium://xxxx">`
-  - Android 使用：`<link rel="manifest" href="https://xxxx/manifest.json">`
-  - manifest 的 url 必须是 https的
+- `<meta name="apple-itunes-app"` 必须填写。
+- `<link rel="manifest"` 和 `android-scheme` 至少填写一个。
+- `<link rel="manifest"` url 必须是 https的。
+- `android-scheme` 和 `android-scheme` 必须同时存在。
 
-- manifest.json 示例
-
+### manifest.json 示例
+<!-- TODO 需要修改，这不符合规范 -->
 ```
 {
   "prefer_related_applications": true, 
   "related_applications": [
     {
-      "platform": "play",
+      "platform": "mip-app-banner",
       "open": "scheme://xx",
       "install": "your download url"
     }
--- src/mip-app-banner/mip-app-banner.js
+++ src/mip-app-banner/mip-app-banner.js
@@ -5,210 +5,271 @@
  */
 
 define(function (require) {
-
-    var customElement = require('customElement').create();
+    var appBannerElement = require('customElement').create();
     var fetchJsonp = require('fetch-jsonp');
     var viewer = require('viewer');
     var util = require('util');
 
     var platform = util.platform;
 
-    var openButton = {
-        setup: function (openButton, openInAppUrl, installAppUrl) {
-            var self = this;
-            openButton.addEventListener('click', function () {
-                self.onClick(openInAppUrl, installAppUrl);
+    appBannerElement.prototype.jumpToLink = function (openInAppUrl, installAppUrl) {
+        var timer = setTimeout(function () {
+            window.top.location.href = installAppUrl;
+            logg('页面失焦1，猜测调起成功');
+            clearTimeout(timer);
+        }, 2500);
+
+        window.open(openInAppUrl, '_top');
+
+        var visibilitychange = function () {
+            logg('页面失焦2，猜测调起成功');
+            var tag = document.hidden || document.webkitHidden;
+            tag && clearTimeout(timer);
+        };
+
+        document.addEventListener('visibilitychange', visibilitychange, false);
+        document.addEventListener('webkitvisibilitychange', visibilitychange, false);
+        window.addEventListener('pagehide', function () {
+            logg('页面失焦3，猜测调起成功');
+            clearTimeout(timer);
+        }, false);
+    };
+
+    /**
+     * 非iframe下，safari 会根据 <meta name="apple-itunes-app">
+     * 此时不显示 MIP 组件banner。
+     *
+     * Android Chrome 虽然会根据 manifest.json 显示系统横幅，但条件较为苛刻，还是以组件调起为准。
+     * manifest.json 显示系统横幅方法：https://github.com/mipengine/mip-extensions/issues/1026#issuecomment-363396075
+     * > 25.0 chrome 可以使用chrome intent吊起App，所以高版本chrome不移除banner
+     * 低版本chrome 可以使用manifest或scheme调起，也不移除banner。
+     *
+     * @return {boolean} 是否具有系统默认Banner
+     */
+    appBannerElement.prototype.hasSystemBanner = function () {
+        // Safari
+        var isSafari = platform.isSafari();
+        if (!viewer.isIframed && isSafari && this.opt.iosMetaTag) {
+            logg('safari 系统默认显示下载');
+            return true;
+        }
+
+        // 其他浏览器，及iframe内打开MIP页的所有情况，需要展示组件Banner
+        logg('无原生默认banner, 显示app-banner');
+        return false;
+    };
+
+    appBannerElement.prototype.initIosBanner = function () {
+        var openInAppUrl = '';
+        var iosInstallUrl = '';
+
+        if (platform.isWechatApp()) {
+            // 获取微信中跳转链接-在Safari中打开或应用宝下载链接。
+            // 由于safari中打开后可以直接调起应用，优先级高于应用宝。
+            var wechatOpenSafari = this.opt.wechatOpenSafari;
+            var wechatIosYyb = this.opt.wechatIosYyb;
+            logg('ios 在微信中');
+            openInAppUrl = wechatOpenSafari || wechatIosYyb;
+        }
+        else {
+            // 拼接schema调起链接
+            var content = this.opt.iosMetaTag && this.opt.iosMetaTag.getAttribute('content');
+            var parts = content.replace(/\s/, '').split(',');
+            var config = {};
+            parts.forEach(function (part) {
+                var params = part.split('=');
+                config[params[0]] = params[1];
             });
-        },
-        onClick: function (openInAppUrl, installAppUrl) {
-            var timer = setTimeout(function () {
-                window.top.location.href = installAppUrl;
-                clearTimeout(timer);
-            }, 1500);
-
-            window.open(openInAppUrl, '_top');
-
-            var visibilitychange = function () {
-                var tag = document.hidden || document.webkitHidden;
-                tag && clearTimeout(timer);
-            };
-
-            document.addEventListener('visibilitychange', visibilitychange, false);
-            document.addEventListener('webkitvisibilitychange', visibilitychange, false);
-            window.addEventListener('pagehide', function () {
-                clearTimeout(timer);
-            }, false);
-        }
-    };
-
-    var ls = {
-        getSotrageKey: function (id) {
-            return 'mip-app-banner:' + id;
-        },
-        hasItem: function (id) {
-            return !!localStorage.getItem(ls.getSotrageKey(id));
-        },
-        setSotrage: function (id) {
-            localStorage.setItem(this.getSotrageKey(id), true);
-        }
-    };
-
-    var dismissButton = {
-        element: null,
-        add: function (element) {
-            this.element = element;
-            var dismissBtn = document.createElement('span');
-            dismissBtn.classList.add('mip-app-banner-dismiss-button');
-            dismissBtn.addEventListener('click', this.onClick.bind(this));
-            element.appendChild(dismissBtn);
-        },
-        onClick: function () {
-            ls.setSotrage(this.element.id);
-            this.element.remove();
-        }
-    };
-
-    var preProcess = {
-        isDismissed: function (id) {
-            return ls.hasItem(id);
-        },
-        init: function (element) {
-            if (this.isDismissed(element.id)) {
-                element.remove();
-                return;
-            }
-            util.css(element, {
-                visibility: 'visible'
-            });
-            dismissButton.add(element);
-        }
-    };
-
-
-    function canShowBanner() {
-        this.isSysBanner = platform.isSafari() || platform.isBaidu();// || platform.isQQ();
-        this.showSysBanner = !viewer.isIframed && this.isSysBanner;
-        if (this.showSysBanner) {
-            return false;
-        }
-
-        this.isEmbeddedSafari = viewer.isIframed && this.isSysBanner;
-        if (this.isEmbeddedSafari) {
-            return false;
-        }
-
-        this.metaTag = document.head.querySelector('meta[name=apple-itunes-app]');
-        if (!this.metaTag) {
-            return false;
-        }
-
-        return true;
-    }
-
-    function iosAppBanner() {
-        var self = this;
-
-        if (!canShowBanner.call(self)) {
-            this.element.remove();
-            return;
-        }
-        this.openButton = this.element.querySelector('button[open-button]');
-        preProcess.init(this.element);
-
-        var content = this.metaTag.getAttribute('content');
-        var parts = content.replace(/\s/, '').split(',');
-        var config = {};
-        parts.forEach(function (part) {
-            var params = part.split('=');
-            config[params[0]] = params[1];
-        });
-
-        var appId = config['app-id'];
-        var openUrl = config['app-argument'];
-        var installAppUrl = 'https://itunes.apple.com/us/app/id' + appId;
-        var openInAppUrl = openUrl || installAppUrl;
-
-        openButton.setup(this.openButton, openInAppUrl, installAppUrl);
-    }
-
-    function andriodAppBanner() {
-        var self = this;
-
-        if (!canShowBanner.call(self)) {
-            this.element.remove();
-            return;
-        }
-        this.openButton = this.element.querySelector('button[open-button]');
-        preProcess.init(this.element);
-
-        this.manifestLink = null;
-        this.manifestHref = '';
-        this.missingDataSources = false;
-
-        this.manifestLink = document.head.querySelector('link[rel=manifest],link[rel=origin-manifest]');
-
-        var isChromeAndroid = platform.isAndroid() && platform.isChrome();
-        var showSysBanner = !viewer.isIframed && isChromeAndroid;
-        if (showSysBanner) {
-
-            this.element.remove();
-            return;
-        }
-
-        this.missingDataSources = platform.isAndroid() && !this.manifestLink;
-        if (this.missingDataSources) {
-            this.element.remove();
-            return;
-        }
-
-        this.manifestHref = this.manifestLink.getAttribute('href');
-        if (/http:\/\//.test(this.manifestHref)) {
-            console.error('必须是https的连接');
-        }
-
-        fetchJsonp(this.manifestHref).then(function (res) {
-            return res.json();
-        }).then(function (data) {
-            var apps = data.related_applications;
-            if (!apps) {
-                self.element.remove();
-            }
-            for (var i = 0; i < apps.length; i++) {
-                var app = apps[i];
-                if (app.platform === 'play') {
-                    var installAppUrl = app.install;
-                    var openInAppUrl = app.open;
-                    openButton.setup(self.openButton, openInAppUrl, installAppUrl);
+            // 优先使用universalLink 调起应用链接
+            var openInAppUrl = platform.getOsVersion().split('.')[0] >= 9 ? this.opt.iosUniversalLink : '';
+            if (!openInAppUrl) {
+                // Fallback 通用方案：拼接schema调起ios应用链接
+                var iosSchemaLink = config['app-argument'];
+                if (iosSchemaLink) {
+                    openInAppUrl = iosSchemaLink;
                 }
             }
-        });
+            // 拼接appStore 下载应用跳转链接
+            iosInstallUrl = 'https://itunes.apple.com/cn/app/id' + config['app-id'];
+        }
 
-    }
+        // 绑定点击-跳转事件
+        var appBanner = this;
+        var openButton = this.element.querySelector('[open-button]');
+        openButton.addEventListener('click', function () {
+            appBanner.jumpToLink(openInAppUrl, iosInstallUrl);
+        });
+        logg('ios 调起链接：' + openInAppUrl);
+        logg('ios 下载链接: ' + iosInstallUrl);
+    };
+
+    appBannerElement.prototype.initAndroidBanner = function () {
+        var openInAppUrl = '';
+        var androidInstallUrl = '';
+        var appBanner = this;
+
+        if (platform.isWechatApp()) {
+            // 获取微信中跳转链接-跳转到应用宝下载
+            var wechatAndroidYyb = this.opt.wechatAndroidYyb;
+            logg('安卓：在微信中');
+            openInAppUrl = wechatAndroidYyb;
+        }
+        else {
+            
+            // 由于manifest优先级高，需要插入到跳转链接第一位。
+            // manifest配置是异步返回，余下appLink，chromeIntent操作需要置于请求处理之后
+            var manifestPromise = new Promise(function (resolve, reject) {
+                if (!appBanner.opt.androidManifest) {
+                    reject('manifest 不存在');
+                    return;
+                }
+
+                // 判定manifest.json 地址为https, 强制要求。
+                var href = appBanner.opt.androidManifest.getAttribute('href');
+                if (/http:\/\//.test(href)) {
+                    reject('manifest必须是https的连接');
+                    return;
+                }
+
+                // 异步请求manifest.json文件，解析install、open关键字
+                fetchJsonp(href).then(function (res) {
+                    return res.json();
+                }).then(function (data) {
+                    var apps = data.related_applications;
+                    if (!apps) {
+                        reject('manifest 不合法，无 related_applications');
+                        return;
+                    }
+                    for (var i = 0; i < apps.length; i++) {
+                        var app = apps[i];
+                        // 'play'实际上为google play, 历史原因不能删除。
+                        // 文档及后续推广一律改为 'mip-app-banner'
+                        if (app.platform === 'play' || app.platform === 'mip-app-banner') {
+                            // androidInstallUrl - 下载链接
+                            // openInAppArr - 调起链接
+                            androidInstallUrl = app.install;
+                            openInAppUrl = app.open;
+                            resolve();
+                            logg('manifest 获取跳转链接完成');
+                        }
+                    }
+                }, function () {
+                    reject('manifest.json 数据格式不合法');
+                });
+            });
+
+            manifestPromise.then(function () {
+                // dep/promise不支持finally, 只能再then一下
+                return new Promise(function(res, rej) {
+                    res();
+                });
+            }, function (error) {
+                console.warn('mip-app-banner解析manifest失败：' + error);
+                // dep/promise不支持finally, 只能再then一下
+                return new Promise(function(res, rej) {
+                    res();
+                });
+            }).then(function () {
+                // ChromeIntent 要求安卓chrome版本高于25
+                var canUseChromeIntent = platform.isChrome()
+                    && navigator.userAgent.match(/Chrome\/(\d+)/)[1] > 25
+                    && appBanner.opt.androidChromeIntent;
+                // AndroidAppLink 要求安卓系统版本高于6.0
+                var canUseAndroidAppLink = navigator.userAgent.match(/Android ([\d.]+)/)
+                    && navigator.userAgent.match(/Android ([\d.]+)/)[1] >= '6.0'
+                    && appBanner.opt.androidAppLink;
+                if (canUseChromeIntent) {
+                    openInAppUrl = appBanner.opt.androidChromeIntent;
+                }
+                else if (canUseAndroidAppLink) {
+                    openInAppUrl = appBanner.opt.androidAppLink;
+                }
+                else if (!openInAppUrl){
+                    // 如果从manifest未读取应用下载链接, 保底方法scheme调起
+                    openInAppUrl = appBanner.opt.androidScheme;
+                }
+
+                // 如果从manifest未读取应用下载链接，则使用用户配置的"android-download"字段
+                if (!androidInstallUrl) {
+                    androidInstallUrl = appBanner.opt.androidDownload;
+                }
+                // 绑定点击-跳转事件
+                var openButton = appBanner.element.querySelector('[open-button]');
+                openButton.addEventListener('click', function () {
+                    appBanner.jumpToLink(openInAppUrl, androidInstallUrl);
+                });
+                logg('android openInAppUrl：' + openInAppUrl);
+                logg('android androidInstallUrl: ' + androidInstallUrl);
+            });
+        }
+    };
 
     /**
      * 构造元素，只会运行一次
      */
-    customElement.prototype.firstInviewCallback = function () {
-        var self = this;
-        var element = self.element;
-        preProcess.isDismissed(element.id);
+    appBannerElement.prototype.firstInviewCallback = function () {
+        var element = this.element;
 
-        util.css(this.element, {
-            display: '',
-            visibility: 'hidden'
-        });
+        // 解析用户配置
+        var jsonScript = element.querySelector('script[type="application/json"]');
+        var jsonString = jsonScript ? jsonScript.innerHTML : '';
+        var customOpt;
+        try {
+            customOpt = JSON.parse(jsonString);
+        }
+        catch (err) {
+            console.warn('<mip-app-banner>配置不是合法JSON, ' + err.message);
+            return;
+        }
+        this.opt = {
+            iosMetaTag: document.head.querySelector('meta[name="apple-itunes-app"]'),
+            iosUniversalLink: customOpt && customOpt['ios-universal-link'],
+            androidManifest: document.head.querySelector('link[rel="manifest"]'),
+            androidScheme: customOpt && customOpt['android-scheme'],
+            androidDownload: customOpt && customOpt['android-download'],
+            androidAppLink: customOpt && customOpt['android-app-link'],
+            androidChromeIntent: customOpt && customOpt['android-chrome-intent'],
+            wechatOpenSafari: customOpt && customOpt['wechat-open-safari'],
+            wechatIosYyb: customOpt && customOpt['wechat-ios-yyb'],
+            wechatAndroidYyb: customOpt && customOpt['wechat-android-yyb']
+        };
+        console.log(this.opt);
 
-        self.openButton = null;
-        self.canShowBanner = null;
-        self.dismissButton = null;
+        // safari，android chrome, 会出现系统 banner
+        if (this.hasSystemBanner()) {
+            logg('存在原生 banner，移除组件');
+            this.element.remove();
+            return;
+        }
 
+        // 初始化Banner
         if (platform.isIos()) {
-            iosAppBanner.call(this);
+            this.initIosBanner();
+            logg('初始化ios banner');
         }
-        else {
-            andriodAppBanner.call(this);
-        }
-    };
+        else if (platform.isAndroid()) {
+            this.initAndroidBanner();
+            logg('初始化安卓 banner');
 
-    return customElement;
+        }
+
+        // 显示banner
+        util.css(element, {
+            display: '',
+            visibility: 'visible'
+        });
+        logg('banner 初始化完成，显示给用户');
+    };
+    return appBannerElement;
+
+    function logg(arr) {
+        var loggContainer = document.getElementById('logg');
+        if (!loggContainer) {
+            loggContainer = document.createElement('div');
+            loggContainer.id = 'logg';
+            loggContainer.setAttribute('style', 'display: block; position: fixed; bottom:0px; opacity: 0.8; background: #ddd; color: black; width: 100%; padding: 10px; max-height: 100px; overflow-y: scroll;');
+            document.body.appendChild(loggContainer);
+        }
+        loggContainer.innerHTML += arr + '<br>';
+    }
 });
